64c606902bf77fbb76289a146e6633ce005d5694
Sebastian Orozco <sebastianorozcogomez@gmail.com>
2024-02-20 02:55:37 -0500

    hotfix

--- next.config.mjs
+++ next.config.mjs
@@ -1,26 +1,14 @@
 /** @type {import('next').NextConfig} */
-
-const isGithubActions = process.env.GITHUB_ACTIONS || false;
-
-let assetPrefix = "";
-let basePath = "/";
-
-if (isGithubActions) {
-  const repo = process.env.GITHUB_REPOSITORY.replace(/.*?\//, "");
-  assetPrefix = `/${repo}/`;
-  basePath = `/${repo}`;
-}
-
 const nextConfig = {
-  assetPrefix: assetPrefix,
-  basePath: basePath,
-  output: "export",
-  reactStrictMode: true,
-  experimental: {
-    appDir: true,
-  },
+  basePath: "/Weather-app-next.js",
   images: {
-    unoptimized: true,
+    remotePatterns: [
+      {
+        protocol: "https",
+        hostname: "openweathermap.org",
+        pathname: "**",
+      },
+    ],
   },
 };
 
