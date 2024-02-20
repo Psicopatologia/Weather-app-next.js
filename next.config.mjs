1aa74b3d955b9d8f7ab3a00baf287767738953e3
Sebastian Orozco <sebastianorozcogomez@gmail.com>
2024-02-20 02:25:05 -0500

    Github actions config

--- next.config.mjs
+++ next.config.mjs
@@ -1,6 +1,19 @@
 /** @type {import('next').NextConfig} */
+
+const isGithubActions = process.env.GITHUB_ACTIONS || false;
+
+let assetPrefix = "";
+let basePath = "/";
+
+if (isGithubActions) {
+  const repo = process.env.GITHUB_REPOSITORY.replace(/.*?\//, "");
+  assetPrefix = `/${repo}/`;
+  basePath = `/${repo}`;
+}
+
 const nextConfig = {
-  basePath: "/Weather-app-next.js",
+  assetPrefix: assetPrefix,
+  basePath: basePath,
   output: "export",
   reactStrictMode: true,
   experimental: {
